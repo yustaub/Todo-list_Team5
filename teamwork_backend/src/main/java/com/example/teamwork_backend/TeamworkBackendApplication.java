92caeb1bb44a0f686f440dc2d5e7d8329f059225
staub <1034397129@qq.com>
2020-04-15 23:08:42 +0800

    initial backend framework

---
+++ teamwork_backend/src/main/java/com/example/teamwork_backend/TeamworkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamwork_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamworkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamworkBackendApplication.class, args);
+	}
+
+}
---
+++ teamwork_backend/src/test/java/com/example/teamwork_backend/TeamworkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamwork_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamworkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
