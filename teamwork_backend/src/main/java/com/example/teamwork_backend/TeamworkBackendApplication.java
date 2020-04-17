5196c1688cb5208bcfe106937ac0bc1efc60d2e2
staub <1034397129@qq.com>
2020-04-17 15:57:20 +0800

    update store

--- teamwork_backend/src/main/java/com/example/teamwork_backend/TeamworkBackendApplication.java
+++ teamwork_backend/src/main/java/com/example/teamwork_backend/TeamworkBackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TeamworkBackendApplication {
 
 	public static void main(String[] args) {
+		System.out.println("rukou");
 		SpringApplication.run(TeamworkBackendApplication.class, args);
 	}
 
