95bd493401dcb6e7abe7422e40f3a67f5f0fd86c
韩洁 <h1603459540@163.com>
2020-04-20 21:44:13 +0800

    add getTodosTest

--- teamwork_frontend/src/App.test.js
+++ teamwork_frontend/src/App.test.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import {
+  render,
+  act,
+  wait,
+  getByTestId,
+} from "@testing-library/react";
+import App from "./App";
+import * as TodoApi from "./api/TodoApi";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("<App>", () => {
+  const item = { id: 1, content: "Team5 homework", createAt: "2020/04/20" };
+
+  beforeEach(() => {
+    jest
+      .spyOn(TodoApi, "getTodos")
+      .mockImplementation(() => Promise.resolve([item]));
+  });
+
+  test("should display todo items correctly", async () => {
+    await act(async () => {
+      render(<App />);
+    });
+
+    expect(getByTestId(document.body, "task-item")).toHaveTextContent(
+      "Team5 homework"
+    );
+  });
 });
