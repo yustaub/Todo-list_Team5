d2a74f86b2d2afed80fe0ea2a0b4c0b4eb50ca6a
xiaodidada <2565960363@qq.com>
2020-04-20 17:40:32 +0800

    TaskController-getAll

--- teamwork_backend/src/main/java/com/example/teamwork_backend/controller/TaskController.java
+++ teamwork_backend/src/main/java/com/example/teamwork_backend/controller/TaskController.java
@@ -1,6 +1,8 @@
 package com.example.teamwork_backend.controller;
 
+
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 
 import com.example.teamwork_backend.model.Task;
@@ -10,12 +12,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -50,5 +56,9 @@ public class TaskController
         }else{
             return ResponseEntity.notFound().build();
         }
+    }
+    @GetMapping(produces = "application/json")
+    public List<Task> getAllTasks(){
+        return taskService.getAll();
     }
 }
