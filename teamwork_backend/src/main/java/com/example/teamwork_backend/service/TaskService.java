315fc5da0976ab7a00519cdc2ff57021ca6bf8e0
xiaodidada <2565960363@qq.com>
2020-04-20 14:01:47 +0800

    TaskService-delete

--- teamwork_backend/src/main/java/com/example/teamwork_backend/service/TaskService.java
+++ teamwork_backend/src/main/java/com/example/teamwork_backend/service/TaskService.java
@@ -28,4 +28,14 @@ public class TaskService{
         }
         return null;
     }
+ 
+    public Optional<Task> delete(Long id) {
+        List<Task> tasks = store.readTasks();
+        Optional<Task> any = tasks.stream().filter(task1 -> task1.getId() == id).findAny();
+        if (any.isPresent()) {//check if ID exists
+            store.writeTasks(tasks.stream().filter(task -> task.getId() != id).collect(Collectors.toList()));
+            return any;
+        }
+        return any;
+    }
 }
