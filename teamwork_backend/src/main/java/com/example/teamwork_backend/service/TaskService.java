859dcaab46d5331d699b5dc025cf3bde0e5ebd96
xiaodidada <2565960363@qq.com>
2020-04-20 12:03:24 +0800

    TaskService-saveTask

--- teamwork_backend/src/main/java/com/example/teamwork_backend/service/TaskService.java
+++ teamwork_backend/src/main/java/com/example/teamwork_backend/service/TaskService.java
@@ -2,6 +2,8 @@ package com.example.teamwork_backend.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import com.example.teamwork_backend.model.Task;
 import com.example.teamwork_backend.store.TaskStore;
@@ -12,14 +14,18 @@ import org.springframework.stereotype.Service;
 public class TaskService{
     @Autowired TaskStore store;
     public Task saveTask(Task task) {
-        List<Task> tasks= new ArrayList<Task>();
         //get all tasks from file
+        List<Task> tasks= new ArrayList<Task>();
         tasks = store.readTasks();
-        task.setUpdatedAt();
-        tasks.add(task);
-        //save all into file
-        store.writeTasks(tasks);
-
-        return task;
+        //check if ID exists
+        Optional<Task> any = tasks.stream().filter(task1 -> task1.getId() == task.getId()).findAny();
+        if (!any.isPresent()) {//id does not exist
+            task.setUpdatedAt();
+            tasks.add(task);
+            //save all into file
+            store.writeTasks(tasks);
+            return task;
+        }
+        return null;
     }
 }
